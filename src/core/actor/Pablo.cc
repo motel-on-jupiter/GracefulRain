4ebd0e08306ac91661bf1bae7a425a542b19c53b
bobozeta <bobo_zeta@motel-on-jupiter.sakuraweb.com>
2015-02-22 11:53:06 +0900

    Change ripple color

--- src/core/actor/Pablo.cc
+++ src/core/actor/Pablo.cc
@@ -2,11 +2,10 @@
  * Copyright (C) 2014 The Motel On Jupiter
  */
 #include "core/actor/Pablo.h"
-#include "mojgame/sampler/color_sample.h"
 
 const float Pablo::kSpeed = 0.05f;
 const float Pablo::kStepLength = 0.013f;
 const float Pablo::kFeetMargin = 0.004f;
-const glm::vec3 Pablo::kStimulusColor(0.0f, 0.2f, 1.0f);
+const glm::vec3 Pablo::kStimulusColor(0.23f, 0.37f, 0.8f);
 const float Pablo::kStimulusEffect = 0.9f;
 
--- src/core/actor/Rina.cc
+++ src/core/actor/Rina.cc
@@ -2,10 +2,9 @@
  * Copyright (C) 2014 The Motel On Jupiter
  */
 #include "core/actor/Rina.h"
-#include "mojgame/sampler/color_sample.h"
 
 const float Rina::kSpeed = 0.05f;
 const float Rina::kStepLength = 0.013f;
 const float Rina::kFeetMargin = 0.004f;
-const glm::vec3 Rina::kStimulusColor(1.0f, 0.2f, 0.2f);
+const glm::vec3 Rina::kStimulusColor(1.0f, 0.27f, 0.0f);
 const float Rina::kStimulusEffect = 0.9f;
